4a49192e3a8ff5a1cc1a6ed6bdcd5783ed10049d
Robert Lutz <robertlutzi@gmail.com>
2023-09-23 13:16:18 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Robert Lutz on 9/23/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
