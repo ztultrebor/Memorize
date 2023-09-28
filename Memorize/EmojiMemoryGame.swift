a73c8804299429a28aaaf96a2b446d4fb46489e8
Robert Lutz <robertlutzi@gmail.com>
2023-09-28 14:11:07 -0400

    add ViewModel

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,14 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Robert Lutz on 9/28/23.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    var model: MemoryGame<String> {
+        
+    }
+}
